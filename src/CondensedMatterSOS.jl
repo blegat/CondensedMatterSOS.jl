7c649d15fdcca05b7c9672c9fad211dfe497dbb6
Benoît Legat <benoit.legat@gmail.com>
2020-05-14 13:34:56 +0200

    Correct fix

--- src/CondensedMatterSOS.jl
+++ src/CondensedMatterSOS.jl
@@ -10,13 +10,15 @@ include("types.jl")
 function Base.convert(::Type{TT}, t::TT) where {T, TT <: AbstractTerm{T}}
     return t
 end
-function Base.promote_rule(::Type{TT}, t::TT) where {T, TT <: AbstractTerm{T}}
-    return t
+# Needed in Julia v1.0
+function Base.promote_rule(TT::Type{<:AbstractTerm{T}}, ST::Type{<:AbstractTermLike{S}}) where {S, T}
+    U = promote_type(S, T)
+    UT = termtype(ST, U)
+    if UT != termtype(TT, U)
+        error("Cannot promote `$ST` and `$TT` to the same type.")
+    end
+    return UT
 end
-# Needed in Julia v1.0 to avoid clashin with promotion.jl:236.
-# See https://travis-ci.com/github/blegat/CondensedMatterSOS.jl/jobs/333686069
-Base.promote_rule(::Type{PT}, ::Type{Any}) where {PT<:MP.APL} = MP.promote_rule_constant(Any, PT)
-MP.promote_rule_constant(::Type{T}, ::Type{TT}) where {T, S, TT<:AbstractTerm{S}} = termtype(TT, promote_type(T, S))
 
 function MP.name_base_indices(var::SpinVariable) # Used to print variable
     splits = split(NAMES[var.id], r"[\[,\]]\s*", keepempty=false)
