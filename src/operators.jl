6dd2cc3ad72cd6467ab3bdb25014bab7ff6d23a9
Cristian <ecboghiu@gmail.com>
2020-05-28 22:25:05 +0200

    Removed the unneded methods for ==.

--- src/operators.jl
+++ src/operators.jl
@@ -68,9 +68,6 @@ function Base.:(==)(a::SpinMonomial, b::SpinMonomial)
 end
 
 function Base.:(==)(var::SpinVariable, term::SpinTerm{T}) where T
-    # NOTE that 1*ax and ax are different types. One is SpinVariable{Int64}
-    # and the other is SpinVarible. Also note that ax*bx*bx=ax is of type
-    # SpinVariable{bool}.
     if isone(coefficient(term))
         return var == term.monomial
     else
@@ -80,39 +77,4 @@ end
 
 function Base.:(==)(term::SpinTerm{T}, var::SpinVariable) where T
     return var == term
-end
-
-function MP.variable(mon::SpinMonomial)
-    if isone(length(mon.variables))
-        return first(values(mon.variables))
-    else
-        MP._errormono2var()
-    end
-end
-
-function Base.:(==)(var::SpinVariable, mon::SpinMonomial)
-    # If there is more than one SpinVariable in SpinMonomial then they are
-    # unequal
-    if !isone(length(mon.variables))
-        return false
-    else
-        return var ==  MP.variable(mon)
-    end
-end
-
-
-function Base.:(==)(mon::SpinMonomial, var::SpinVariable)
-    return var == mon
-end
-
-function Base.:(==)(mon::SpinMonomial, term::SpinTerm{T}) where T
-    if isone(coefficient(term))
-        return mon == term.monomial
-    else
-        return false
-    end
-end
-
-function Base.:(==)(term::SpinTerm{T}, mon::SpinMonomial) where T
-    return mon == term
 end
