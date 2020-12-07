7d94a8da89fcc042dd17ff05a99abe2d50a1a895
Benoît Legat <benoit.legat@gmail.com>
2020-12-07 10:22:52 +0100

    Ising with SumOfSquares (#14)
    * Ising with SumOfSquares
    
    * Update
    
    * Convert to Literate
    
    * Avoid double export
    
    * Fixes

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # Condensed Matter with Sum-of-Squares
 
-| **Build Status** |
-|:----------------:|
-| [![Build Status][build-img]][build-url] [![Codecov branch][codecov-img]][codecov-url] |
+| **Documentation** | **Build Status** |
+|:-----------------:|:----------------:|
+| | [![Build Status][build-img]][build-url] |
+| [![][docs-latest-img]][docs-latest-url] | [![Codecov branch][codecov-img]][codecov-url] |
+
+[docs-latest-img]: https://img.shields.io/badge/docs-latest-blue.svg
+[docs-latest-url]: https://blegat.github.io/CondensedMatterSOS.jl/dev
 
 [build-img]: https://api.travis-ci.com/blegat/CondensedMatterSOS.jl.svg?branch=master
 [build-url]: https://travis-ci.com/github/blegat/CondensedMatterSOS.jl
---
+++ docs/make.jl
@@ -0,0 +1,35 @@
+using Documenter
+using Literate
+using CondensedMatterSOS
+using Test
+
+const EXAMPLES_DIR = joinpath(@__DIR__, "..", "examples")
+const OUTPUT_DIR   = joinpath(@__DIR__, "src/generated")
+
+include(joinpath(EXAMPLES_DIR, "run_examples.jl"))
+
+for example in EXAMPLES
+    example_filepath = joinpath(EXAMPLES_DIR, example)
+    Literate.markdown(example_filepath, OUTPUT_DIR)
+    Literate.notebook(example_filepath, OUTPUT_DIR)
+end
+
+makedocs(
+    sitename = "CondensedMatterSOS",
+    # See https://github.com/JuliaDocs/Documenter.jl/issues/868
+    format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
+    # See https://github.com/jump-dev/JuMP.jl/issues/1576
+    strict = true,
+    pages = [
+        "Introduction" => "index.md",
+        "Examples" => map(EXAMPLES) do jl_file
+            # Need `string` as Documenter fails if `name` is a `SubString{String}`.
+            name = string(split(jl_file, ".")[1])
+            return name => "generated/$name.md"
+        end
+    ],
+)
+
+deploydocs(
+    repo   = "github.com/blegat/CondensedMatterSOS.jl.git",
+)
